3d0a13c60d445d4b99cef93d39c6bb3dd8be070c
Marco A S Azevedo <37423042+marcoazevedo01@users.noreply.github.com>
2020-06-07 13:56:23 -0300

    v0.11

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./config/server');
 
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 3000;
 
 app.listen(port, function(){
     console.log('server listen port ',port);
