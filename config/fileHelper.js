33e1cbcb8718242bc3475a0c72225f1a2205d940
Marco A S Azevedo <37423042+marcoazevedo01@users.noreply.github.com>
2020-06-09 19:56:19 -0300

    module6

--- config/fileHelper.js
+++ config/fileHelper.js
@@ -10,30 +10,18 @@ exports.compressImage = (file, size) => {
         .webp({quality: 100})
         .toBuffer()
         .then(data => {
-
             fs.access(file.path, (err) => {
-
-                // Um erro significa que a o arquivo não existe, então não tentamos apagar
                 if (!err) {
-                    
-                    //Se não houve erros, tentamos apagar
                     fs.unlink(file.path, err => {
-
-                        // Não quero que erros aqui parem todo o sistema, então só vou imprimir o erro, sem throw.
                         if(err) console.log(err)
                     })
                 }
             });
-            
-            //Agora vamos armazenar esse buffer no novo caminho
             fs.writeFile(newPath, data, err => {
                 if(err){
-                    // Já aqui um erro significa que o upload falhou, então é importante que o usuário saiba.
                     throw err;
                 }
             });
-
-            // Se o código chegou até aqui, deu tudo certo, então vamos retornar o novo caminho
             return newPath;
         })
 }
