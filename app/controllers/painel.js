f1ec6890f38f0c691daa7806f6bc7cb655f40392
Marco A S Azevedo <37423042+marcoazevedo01@users.noreply.github.com>
2020-06-09 14:53:09 -0300

    estavel

--- app/controllers/painel.js
+++ app/controllers/painel.js
@@ -8,7 +8,8 @@ class PainelControll {
         return {
             authenticate: '/painel*',
             list: '/painel/:id',
-            remove: '/painel/post/:id'
+            edit: '/painel/post/:id',
+            remove: '/painel/post/remove/:id'
         };
     }
 
@@ -16,13 +17,14 @@ class PainelControll {
         return function(req, resp, next) {
             const postDao = new PostDAO(url); 
             postDao.list()
-                .then(postagens => resp.render('./painel',{postagens:postagens}))
+                .then(postagens => resp.render('./painel',{postagens:postagens, dados:{}}))
                 .catch(error => console.log(error)); 
         };
     }  
 
     insert(){
         return function(req, resp) {
+            console.log('no insert');
             const postDao = new PostDAO(url); 
             if(req.file){
                 filehelper.compressImage(req.file, 350)
@@ -48,9 +50,42 @@ class PainelControll {
         }
     }
 
+    formEdit(){
+        return function(req, resp) {
+            let id = req.params.id;
+            const postDao = new PostDAO(url); 
+            postDao.searchId(id)
+                .then(dados => resp.render('./templates/form',{dados:dados}))
+                .catch(error => console.log(error))
+            
+        }
+    }
+
     edit(){
         return function(req, resp) {
-            console.log(req.body);
+            console.log('entrou no edit');
+            const postDao = new PostDAO(url); 
+            if(req.file){
+                filehelper.compressImage(req.file, 350)
+                    .then(newPath => montObj())
+                    .then(obj => postDao.edit(obj))   
+                    .then(suss => resp.redirect('./painel'))         
+                    .catch(err => console.log(err) );
+
+            }else{
+                 return resp.redirect('./painel');
+            }
+
+            function montObj(){
+                let date = new Date();
+                let data = {
+                    'img':req.file.filename.split('.')[0] + '.webp',
+                    'title':req.body.title,
+                    'message':req.body.message,
+                    'date': `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()} - ${date.getHours()}:${date.getMinutes()}`
+                }
+                return data;
+            }
         }
     }
 
--- app/routes/painelR.js
+++ app/routes/painelR.js
@@ -3,23 +3,25 @@ const painelControll = new PainelControll();
 const multer = require('../../config/upload');
 
 module.exports = (app) => {
-    const painelRouts = PainelControll.routs();
+  const painelRouts = PainelControll.routs();
 
-    app.use(painelRouts.authenticate, function(req, resp, next){
-        if(req.isAuthenticated()){
-          //app.locals.user = true; // <<<
-          next();
-        }else{
-          //app.locals.user = false;
-          resp.redirect('/login');
-        }
-    });
-   
-    app.route(painelRouts.list)
-        .get(painelControll.list())
-        .post(multer.single('image'),painelControll.insert())
-        .put(painelControll.edit()) 
-   
-    app.get(painelRouts.remove, painelControll.remove())
+  app.use(painelRouts.authenticate, function (req, resp, next) {
+    if (req.isAuthenticated()) {
+      //app.locals.user = true; // <<<
+      next();
+    } else {
+      //app.locals.user = false;
+      resp.redirect('/login');
+    }
+  });
+
+  app.route(painelRouts.list)
+    .get(painelControll.list())
+    //.post(multer.single('image'), painelControll.insert())
+    .put(painelControll.edit())
+
+  app.get(painelRouts.edit, painelControll.formEdit())
+  app.get(painelRouts.remove, painelControll.remove())
+    
 
 };
